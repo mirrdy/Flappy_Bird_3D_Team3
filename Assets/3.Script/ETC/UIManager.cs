f2b4f2519db7f3bdf07c17bf94f43d6526983678
gure123 <eo2gus66@gmail.com>
2023-05-04 16:19:40 +0900

    Update UIManager.cs

--- Assets/3.Script/ETC/UIManager.cs
+++ Assets/3.Script/ETC/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -15,4 +16,6 @@ public class UIManager : MonoBehaviour
         }
         else Destroy(gameObject);
     }
+
+
 }
