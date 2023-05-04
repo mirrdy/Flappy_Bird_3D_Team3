3d78d5421164fd8b389c947af3b37fe957d9869b
gure123 <eo2gus66@gmail.com>
2023-05-04 16:15:49 +0900

    UIManager

---
+++ Assets/3.Script/ETC/UIManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+}
