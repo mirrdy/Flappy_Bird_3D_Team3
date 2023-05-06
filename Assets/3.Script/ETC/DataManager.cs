c29ec00e3b37b058618f36d386c09608862461b0
gure123 <83895630+gure123@users.noreply.github.com>
2023-05-07 03:24:57 +0900

    UI

--- Assets/3.Script/ETC/DataManager.cs
+++ Assets/3.Script/ETC/DataManager.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class RankingData
 {
-    public List<int> Ranking;
-    public List<string> name;
-    public List<float> time;
+    public int[] Ranking = new int[11];
+    public string[] name = new string[11];
+    public float[] time = new float[11];
 }
 
 public class DataManager : MonoBehaviour
@@ -16,8 +17,11 @@ public class DataManager : MonoBehaviour
 
     RankingData rankingdata = new RankingData();
 
+    [SerializeField] private Text NameText;
+    [SerializeField] private Text ScoreText;
+
     string path;
-    string fileName = "RankingData.json";
+    string fileName = "RankingData";
 
     private void Awake()
     {
@@ -30,34 +34,103 @@ public class DataManager : MonoBehaviour
 
         path = Application.dataPath + "/Resources/";
 
-        rankingdata.Ranking = new List<int>();
-        rankingdata.name = new List<string>();
-        rankingdata.time = new List<float>();
+        for(int i = 0; i < rankingdata.Ranking.Length; i++)
+        {
+            rankingdata.Ranking[i] = 0;
+            rankingdata.name[i] = "---";
+            rankingdata.time[i] = 0f;
+        }
+
+        rankingdata.name[10] = "ȫ�浿";
+        rankingdata.time[10] = 125f;
     }
 
-    public void SaveData(string name, float time)
+    public void Start()
     {
-        string data;
+        SaveData();
+        LoadData();
+    }
 
-        /*rankingdata.name.Add(name);
-        rankingdata.time.Add(time);*/
+    public void SaveData()
+    {
+        UpdateRank();
 
-        data = JsonUtility.ToJson(rankingdata);
-        File.WriteAllText(path + fileName, data);
+        string data = JsonUtility.ToJson(rankingdata);
+        File.WriteAllText(path + fileName + ".json", data);
     }
 
     public void LoadData()
     {
-        string data = File.ReadAllText(path + fileName);
-        for (int i = 0; i < 11; i++)
-        {
-            rankingdata = JsonUtility.FromJson<RankingData>(data);
-        }
+        string data = File.ReadAllText(path + fileName + ".json");
+        rankingdata = JsonUtility.FromJson<RankingData>(data);
     }
 
-    public RankingData ShuffleList(RankingData rankingdata)
+    public RankingData UpdateRank()
     {
+        RankingData ranking = new RankingData();
 
+        for (int i = 0; i < rankingdata.Ranking.Length; i++)
+        {
+            rankingdata.Ranking[i] = 1;
+            for (int j = 0; j < rankingdata.Ranking.Length; j++)
+            {
+                if (rankingdata.time[i] < rankingdata.time[j] || rankingdata.time[i] == 0f)
+                {
+                    rankingdata.Ranking[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < rankingdata.Ranking.Length; i++)
+        {
+            ranking.Ranking[i] = rankingdata.Ranking[i];
+            ranking.name[i] = rankingdata.name[i];
+            ranking.time[i] = rankingdata.time[i];
+        }
+
+        for (int i = 0; i < rankingdata.Ranking.Length; i++)
+        {
+            
+            for (int j = 0; j < rankingdata.Ranking.Length; j++)
+            {
+                if(ranking.Ranking[j] == i + 1)
+                {
+                    rankingdata.Ranking[i] = ranking.Ranking[j];
+                    rankingdata.name[i] = ranking.name[j];
+                    rankingdata.time[i] = ranking.time[j];
+                    rankingdata.name[j] = "---";
+                    rankingdata.time[j] = 0f;
+                    continue;
+                }
+            }
+        }
         return rankingdata;
+    }
+
+    public void Print()
+    {
+        NameText.text = rankingdata.name[0] + "\n" + "\n" +
+                        rankingdata.name[1] + "\n" + "\n" +
+                        rankingdata.name[2] + "\n" + "\n" +
+                        rankingdata.name[3] + "\n" + "\n" +
+                        rankingdata.name[4] + "\n" + "\n" +
+                        rankingdata.name[5] + "\n" + "\n" +
+                        rankingdata.name[6] + "\n" + "\n" +
+                        rankingdata.name[7] + "\n" + "\n" +
+                        rankingdata.name[8] + "\n" + "\n" +
+                        rankingdata.name[9] + "\n" + "\n" +
+                        rankingdata.name[10] + "\n" + "\n";
+
+        ScoreText.text = (int)rankingdata.time[0] / 60 + " : " + (int)rankingdata.time[0] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[1] / 60 + " : " + (int)rankingdata.time[1] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[2] / 60 + " : " + (int)rankingdata.time[2] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[3] / 60 + " : " + (int)rankingdata.time[3] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[4] / 60 + " : " + (int)rankingdata.time[4] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[5] / 60 + " : " + (int)rankingdata.time[5] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[6] / 60 + " : " + (int)rankingdata.time[6] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[7] / 60 + " : " + (int)rankingdata.time[7] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[8] / 60 + " : " + (int)rankingdata.time[8] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[9] / 60 + " : " + (int)rankingdata.time[9] % 60 + "\n" + "\n" +
+                         (int)rankingdata.time[10] / 60 + " : " + (int)rankingdata.time[10] % 60 + "\n" + "\n";
     }
 }
